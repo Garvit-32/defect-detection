ba71d37966b289f2fcb84f994065fc55ce6a7320
Garvit-32 <garvitgarg32@gmail.com>
2021-08-24 13:42:02 +0530

    ppt added

--- debug.py
+++ debug.py
@@ -1,31 +1,35 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import cv2
 
 
-df = pd.read_csv('Train_DefectBoxes_PrithviAI.csv')
+import pandas as pd
+df = pd.read_csv('Train_DefectType_PrithviAI.csv')
+defect = df.loc[df['defect_flag'] == 1]['images id'].values
 
 # print(df.head())
 
 
-val1 = df.loc[df['image_id'] == '000272.png'].values[0][1:]
+# print(df.columns)
 
+
+
+
+# # print(val1)
+# # val1 = df.loc[, :].values[1:]
 # print(val1)
-# val1 = df.loc[, :].values[1:]
-print(val1)
 
-img_sz = (4096, 1000)
+# img_sz = (4096, 1000)
 
 
-img = cv2.imread('Images/000272.png')
+# img = cv2.imread('Images/000272.png')
 
 
-pt1 = (int((val1[0] - val1[2]/2) * 4096), int((val1[1] - val1[3]/2) * 1000))
-pt2 = (int((val1[0] + val1[2]/2) * 4096), int((val1[1] + val1[3]/2) * 1000))
+# pt1 = (int((val1[0] - val1[2]/2) * 4096), int((val1[1] - val1[3]/2) * 1000))
+# pt2 = (int((val1[0] + val1[2]/2) * 4096), int((val1[1] + val1[3]/2) * 1000))
 
 
-img = cv2.rectangle(img, pt1, pt2, (0, 0, 255), 2)
+# img = cv2.rectangle(img, pt1, pt2, (0, 0, 255), 2)
 
-plt.imshow(img[:, :, ::-1])
-plt.show()
+# plt.imshow(img[:, :, ::-1])
+# plt.show()
